06e3fa72f98c85bda36c94c2cd3d7d79e7eee72c
vedant202 <vdhenge202@gmail.com>
2023-10-26 15:46:18 +0530

    XML Views

--- project1/webapp/index.js
+++ project1/webapp/index.js
@@ -1,4 +1,16 @@
-sap.ui.define([], () => {
+sap.ui.define([
+    "sap/m/Text",
+    "sap/ui/core/mvc/XMLView"
+
+], (Text,XMLView) => {
 	"use strict";
 	alert("UI5 is ready");
+
+    // new Text({
+    //     text:"Hello World1"
+    // }).placeAt("content");
+
+    XMLView.create({
+        viewName:"ui5.walkthrough.view.App"
+    }).then((oView)=>oView.placeAt("content"))
 });
